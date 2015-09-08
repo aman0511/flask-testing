8a9fe0282368d6c82839d3e51658351bbc6bb14c
Akshay Pratap Singh <pratapakshay0@gmail.com>
2015-09-08 19:32:25 +0530

    (setup): init setup

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for
+import os
+
+app = Flask(__name__)
+
+# Determines the destination of the build. Only usefull if you're using Frozen-Flask
+app.config['FREEZER_DESTINATION'] = os.path.dirname(os.path.abspath(__file__))+'/../build'
+
+# Function to easily find your assets
+# In your template use <link rel=stylesheet href="{{ static('filename') }}">
+app.jinja_env.globals['static'] = (
+    lambda filename: url_for('static', filename = filename)
+)
+
+# import views
+from app.views import main
+
+# register blueprints
+app.register_blueprint(main.mod)
